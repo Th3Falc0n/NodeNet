5f058ac228cbe233a5cad2828a03392bbedf17e2
Fabian Sachara <Fabian@Sachara.de>
2013-01-15 18:00:47 +0100

    added comments to Ping program

--- src/org/th3falc0n/nodenet/programs/Ping.java
+++ src/org/th3falc0n/nodenet/programs/Ping.java
@@ -17,19 +17,18 @@ public class Ping extends TerminalProgram {
     private static final long serialVersionUID = 1L;
 
     @Override
-    public void handlePacket(Packet pck) throws RemoteException {
-      if(pck.subID == 0) client.send(new Packet(1, 1), pck.fromAddr);
-      if(pck.subID == 1) client.postTerminalMessage(logger, "Pong!");
+    public void handlePacket(Packet pck) throws RemoteException { //Is called if a packet is received for this handler.
+      if(pck.subID == 0) client.send(new Packet(1, 1), pck.fromAddr); //subID 0 is a ping request. Just answer this with a subID 1 (ping answer)
+      if(pck.subID == 1) client.postTerminalMessage(logger, "Pong!"); //subID 1 is a ping answer. Post "Pong!" to terminal stdio.
     }
 
     @Override
-    public void handleCommand(String[] command) throws RemoteException {
-      if(command[1] == null) {
+    public void handleCommand(String[] command) throws RemoteException { //Is called if a terminal command is executed for this handler.
+      if(command[1] == null) { //argument 1 is the address and may not be empty
         postUsage();
         return;
       }
-      String address = command[1];
-      client.send(new Packet(1, 0), address);
+      client.send(new Packet(1, 0), command[1]); //Send ping request packet.
     }
     
     private void postUsage() throws RemoteException {
@@ -38,14 +37,14 @@ public class Ping extends TerminalProgram {
     
   }
   
-  public static void runIntegrated() {
+  public static void runIntegrated() { //runIntegrated is main equivalent for a program which is started from the Terminal thread.
     try {
-      client = NodeNetConnector.getRemoteClient();
+      client = NodeNetConnector.getRemoteClient(); //Get the NodeNet-Client instance from RMI Server
       
-      CallHandler handler = new Ping.CallHandler();
-      client.registerPacketHandler(1, handler);
-      client.registerTerminalCommand("ping", handler);
-      client.postTerminalMessage(logger, "Registered Program");
+      CallHandler handler = new Ping.CallHandler(); //Create instance of CallHandler
+      client.registerPacketHandler(1, handler); //Register packetID 1 (TCP/IP port equivalent) requests to be handled by CallHandler
+      client.registerTerminalCommand("ping", handler); //Register the Terminal command "ping" to be handled by CallHandler
+      client.postTerminalMessage(logger, "Registered Program"); //Post "Registered Program" message to terminal stdio
     }
     catch (Exception e) {
       e.printStackTrace();
